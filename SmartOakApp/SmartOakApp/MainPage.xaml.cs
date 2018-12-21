e7d11ff85019454e5d1cc7bb195c88fecadea9f9
Rentrax <34304517+Rentrax@users.noreply.github.com>
2018-12-21 20:48:59 +0100

    Dodaj pliki projektów.

---
+++ SmartOakApp/SmartOakApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SmartOakApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
